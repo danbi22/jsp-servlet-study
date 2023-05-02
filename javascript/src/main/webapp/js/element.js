aa7439544c5febded552c9a617eac8031375c171
yudahan <103639913+danbi22@users.noreply.github.com>
2023-05-02 11:15:21 +0900

    javascript

--- javascript/src/main/webapp/js/element.js
+++ javascript/src/main/webapp/js/element.js
@@ -1,3 +1,28 @@
 /**
  * element.jss
  */
+
+// HTML 문서에 'DOMContentLoaded' 이벤트 리스너를 등록
+// 브라우저가 HTML 문서의 모든 요소를 생성하고 난 후 
+document.addEventListener('DOMContentLoaded', function () {
+   
+    // id로 HTML 요소를 찾음   
+    const btn1 = document.getElementById('btn1');
+    console.log(btn1);
+    
+    // 찾은 HTML 요소에 'click' 이벤트 리스너 등록
+    // addEventListener('eventName', callback);
+    // callback: 이벤트가 발생했을 때 브라우저가 호출하는 함수. 이벤트를 처리하는 함수.
+    btn1.addEventListener('click', function () {
+        // id="d1"인 요소를 찾아서 바탕색을 변경
+        const d1 = document.getElementById('d1');
+        d1.style.backgroundColor = 'lavender';
+    });
+    
+    // id="btn2" 버튼에 'click' 이벤트 리스너를 등록
+    const btn2 = document.getElementById('btn2');
+    btn2.addEventListener('click', function () {
+        
+    })
+    
+});
