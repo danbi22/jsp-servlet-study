26675c7acb5a7b1b57337a59043aa6fe1b2d6025
yudahan <103639913+danbi22@users.noreply.github.com>
2023-05-02 15:03:12 +0900

    javascript

--- javascript/src/main/webapp/js/event.js
+++ javascript/src/main/webapp/js/event.js
@@ -4,15 +4,47 @@
 
  // btnInput 리스너 달기 -> input#itemInput 내용을 가지고 ul#itemList을 만든다 -> input#itemInput의 내용을 지운다
 document.addEventListener('DOMContentLoaded', function () {
-    const btnInput = document.getElementById('btnInput');
+   /* const btnInput = document.getElementById('btnInput');
     const itemInput = document.getElementById('itemInput');
-    const itemList = document.getElementById('itemList');
+    const itemList = document.getElementById('itemList');*/
+    
+    const itemInput = document.querySelector('input#itemInput');
+    const btnInput= document.querySelector('button#btnInput');
+    const itemList= document.querySelector('ul#itemList');
     
     btnInput.addEventListener('click', function () {
-        let itemInputValue = itemInput.value;
+        // input에 입력된 값을 읽음
+        const itemInputValue = itemInput.value;
         console.log(itemInputValue);
         
-        itemList.innerHTML = itemInputValue;
-        itemInput.innerText = '';
+        // input에 입력된 값으로 li 요소를 만듦
+        const item = `<li>${itemInputValue}</li>`;
+        itemList.innerHTML += item;
+        
+        // input의 값을 지움
+        itemInput.value = '';
+        
+        // input에 포커스를 줌
+        itemInput.focus();
     });
+    
+    const itemInput2 = document.querySelector('input#itemInput2');
+    const itemList2 = document.querySelector('ul#itemList2');
+    
+    // 'keydown'리스너: 키를 누르면 실행
+    itemInput2.addEventListener('keydown', function(event) {
+        // console.log(event); -> event: KeyboardEvent 객체
+        // 
+        if (event.keyCode === 13) {
+            const value = itemInput2.value;
+            const item = `<li>${value}</li>`;
+            itemList2.innerHTML += item;
+            
+          itemInput2.value = '';
+          
+          itemInput2.focus();  
+        }
+    });
+    
+    
 });
