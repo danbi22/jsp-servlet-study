dcd664e32abafb45e38dcfb412483b331b1114a9
yudahan <103639913+danbi22@users.noreply.github.com>
2023-05-12 09:41:47 +0900

    DetailController.java

--- jsp2/src/main/java/com/itwill/post/controller/post/PostDetailController.java
+++ jsp2/src/main/java/com/itwill/post/controller/post/PostDetailController.java
@@ -7,26 +7,51 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.itwill.post.model.Post;
+import com.itwill.post.service.PostService;
+
 /**
  * Servlet implementation class PostDetailController
  */
+@WebServlet(name = "postDetailController", value = {"/post/detail"})
 public class PostDetailController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-
+	
+	private static Logger log = LoggerFactory.getLogger(PostDetailController.class);
+	
+	private static PostService postService = PostService.getPostService();
+	
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+	    log.info("detail: doGet()");
+	    
+	    // 목록 페이지에서 선택된 열의 id를 가져옴
+	    int id = Integer.parseInt(request.getParameter("id"));
+	    
+	    // 선택된 열의 id를 가지고 해당 post를 찾음
+	    Post post = postService.readAt(id); 
+	    log.info("post{}", post);
+	    
+	    
+	    // DB에서 가져온 post를 request에 넣어줌
+	    request.setAttribute("post", post);
+	    
+	    // jsp에 request를 전달
+	    request.getRequestDispatcher("/WEB-INF/post/detail.jsp").forward(request, response);
+	    
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+	    
+		
 	}
 
 }
